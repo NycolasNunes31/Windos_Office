670eabeb8111e98da28f5c54ca77ec11105c1ae9
Nycolas Nascimento <54158708+NycolasNunes31@users.noreply.github.com>
2023-01-25 18:02:27 -0400

    Add files via upload

---
+++ ATIVADOR_WINDOWS_10_E_11.bat
@@ -0,0 +1,8 @@
+
+cscript slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+cscript slmgr.vbs /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+cscript slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+cscript slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+cscript slmgr.vbs /skms kms.msgang.com
+cscript slmgr.vbs /ato
+pause
